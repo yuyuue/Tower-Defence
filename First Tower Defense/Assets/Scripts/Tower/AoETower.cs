0ec2694f985811d39aeaa0ba0d96e83d5ee73898
YuYu <uuprivate224m@gmail.com>
2024-05-15 23:28:57 +0900

    making tower2

--- First Tower Defense/Assets/Scripts/Tower/AoETower.cs
+++ First Tower Defense/Assets/Scripts/Tower/AoETower.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AoETower : MonoBehaviour
+public class AoETower : Tower
 {
-    // Start is called before the first frame update
-    void Start()
+    public LayerMask enemyLayer;  // 敵を検出するためのレイヤーマスク
+
+    protected override void InitializeTower()
     {
-        
+        // 範囲攻撃タワー特有の初期化処理
+        Debug.Log("AoE Tower initialized.");
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Attack()
     {
-        
+        // 範囲内のすべての敵に攻撃を行うロジック
+        Collider[] enemiesInRange = Physics.OverlapSphere(transform.position, range, enemyLayer);
+        foreach (Collider enemy in enemiesInRange)
+        {
+            // ここで各敵にダメージを与える処理を呼び出す
+            Debug.Log("Attacking an enemy with AoE damage.");
+        }
+    }
+
+    protected override bool CanAttack()
+    {
+        // 攻撃可能かどうかを判定するロジック
+        // 範囲内に敵がいるかどうかをチェック
+        Collider[] enemiesInRange = Physics.OverlapSphere(transform.position, range, enemyLayer);
+        return enemiesInRange.Length > 0;
     }
 }
